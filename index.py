eb651a4c14af3ded777696ef0f8f4a0a00bb8ecd
FahadAliHashmi2011 <faiezali2006@gmail>
2025-11-23 15:22:33 +0000

    ss

--- index.py
+++ index.py
@@ -11,3 +11,20 @@ class IOString():
 str1 = IOString()
 str1.get_String()
 str1.print_String()
+#==========================-------------------------------------=============================
+class Employee:
+    def __init__(self):
+        print('Employee created')
+
+    def __del__(self):
+        print("Destructor called")
+
+def Create_obj():
+    print('Making Object...')
+    obj = Employee()
+    print('function end...')
+    return obj
+
+print('Calling Create_obj() function...')
+obj = Create_obj()
+print('Program End...')
